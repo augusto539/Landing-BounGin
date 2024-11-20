0e8bb63f05dc2b326bfe7531ca348d0c58a40c31
augusto <augustoantonelli88@gmail.com>
2024-11-20 19:35:13 -0300

    cambio un nombre

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './Index.css';
+import './index.css';
 import Header from './Secctions/Header/Header';
 import Botanicos from './Secctions/Botanicos/Botanicos';
 
