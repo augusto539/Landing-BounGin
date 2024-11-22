decbab4cc8ab41bc728a5e1c4c3a727c8f6909c3
augusto antonelli <augustoantonelli88@gmail.com>
2024-11-21 21:51:22 -0300

    empiezo la seccion Equipo

---
+++ src/Secctions/Equipo/Equipo.jsx
@@ -0,0 +1,18 @@
+import './Equipo.css';
+
+function Equipo() {
+    return (
+        <div className="Equipo">
+            <div className="izquierda">
+                <img src="" alt="" />
+            </div>
+            <div className="derecha">
+                <p>
+                    Somos un grupo de estudiantes de ingenieria quimica apacionados por los destilados que como poryecto de quimica aplicada decidimos adentrarnos al mundo del gin con nuestro producto "BounGin", que es un gin de gusto y aspecto inovador.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default Equipo;
